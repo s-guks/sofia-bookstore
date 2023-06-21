7130a81d9eafb6ba61b1b4e16cc89b0e66d5f61e
Sofia Gukelberger <sofia.gukelberger@mavenwave.com>
2023-06-21 15:24:58 -0400

    modularized book cards

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+import { BOOKS } from '../mock-books';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {CdkDrag} from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
   styleUrls: ['./book-card.component.scss'],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CdkDrag],
+  imports: [MatCardModule, MatButtonModule, CdkDrag, FormsModule, CommonModule],
 })
 export class BookCardComponent{
 
+    books = BOOKS;
 
 }
 
