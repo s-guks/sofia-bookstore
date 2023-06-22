e395032ff97a0eb2affb067a92e822dbff4a0113
Sofia Gukelberger <sofia.gukelberger@mavenwave.com>
2023-06-22 13:02:29 -0400

    Made cards smaller, expandable, and fixed formatting issues

--- src/app/book-card/book-card.component.ts
+++ src/app/book-card/book-card.component.ts
@@ -3,6 +3,7 @@ import { Book } from '../book';
 import { BOOKS } from '../mock-books';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
 import {CdkDrag} from '@angular/cdk/drag-drop';
 import { FormsModule } from '@angular/forms';
 import {CommonModule} from '@angular/common';
@@ -12,7 +13,7 @@ import {CommonModule} from '@angular/common';
   templateUrl: './book-card.component.html',
   styleUrls: ['./book-card.component.scss'],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CdkDrag, FormsModule, CommonModule],
+  imports: [MatCardModule, MatButtonModule, MatExpansionModule, CdkDrag, FormsModule, CommonModule],
 })
 export class BookCardComponent{
 
