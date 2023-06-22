81a66cbb0d70cbb0eafe6ebc2bbd8256f45fc23d
Sofia Gukelberger <sofia.gukelberger@mavenwave.com>
2023-06-22 14:53:43 -0400

    fixed formatting issues

--- src/app/mock-books.ts
+++ src/app/mock-books.ts
@@ -16,5 +16,13 @@ export const BOOKS: Book[] = [
         desc: "Ged was the greatest sorcerer in Earthsea, but in his youth he was the reckless Sparrowhawk. In his hunger for power and knowledge, he tampered with long-held secrets and loosed a terrible shadow upon the world. This is the tumultuous tale of his testing, how he mastered the mighty words of power, tamed an ancient dragon, and crossed death's threshold to restore the balance.",
         bio: "https://www.ursulakleguin.com/",
         buy: "https://www.amazon.com/Wizard-Earthsea-Cycle/dp/0547773749"
+    },
+    {
+        title: "The Hobbit",
+        author: "J. R. R. Tolkien",
+        cover: "assets/bookcover3.jpeg",
+        desc: "Bilbo Baggins is a hobbit who enjoys a comfortable, unambitious life, rarely traveling any farther than his pantry or cellar. But his contentment is disturbed when the wizard Gandalf and a company of dwarves arrive on his doorstep one day to whisk him away on an adventure. They have launched a plot to raid the treasure hoard guarded by Smaug the Magnificent, a large and very dangerous dragon. Bilbo reluctantly joins their quest, unaware that on his journey to the Lonely Mountain he will encounter both a magic ring and a frightening creature known as Gollum.",
+        bio: "https://www.tolkiensociety.org/",
+        buy: "https://www.amazon.com/Hobbit-J-R-Tolkien/dp/054792822X"
     }
   ];
